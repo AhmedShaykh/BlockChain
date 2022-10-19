70f6ec8b9f59b80c4b34a987192a9ad791d07c1b
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-10-19 14:10:10 +0500

    Git Init

--- TypeScript/src/App.ts
+++ TypeScript/src/App.ts
@@ -4,8 +4,10 @@ const abc: string = "TS";
 const num: number = 123;
 const bool: boolean = true;
 let undef: undefined;
+let nul: null = null;
 
 console.log(abc);
 console.log(num);
 console.log(bool);
 console.log(undef);
+console.log(nul);
