a59596881e5a90bce0138f1a21f141259138474a
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-10-20 22:33:20 +0500

    Git Init

--- TypeScript/src/App.ts
+++ TypeScript/src/App.ts
@@ -36,10 +36,49 @@ import { Course } from "./index";
 
 export const course: Course = {
     name: "Web 3 & Metaverse Developer",
-    classDay: "Webnesday",
+    classDay: "Wednesday",
     startUp: "DAO",
     classTiming: 6
 }
 
 console.log(metaVerse);
 console.log(course);
+
+// ========== Unions ========== //
+
+let mathematician = Math.random() > 0.5 ? 123 : "Mark Goldberg";
+console.log(mathematician);
+
+let thinker: string | null = null;
+
+if (Math.random() > 0.5) {
+    thinker = "DataScript";
+}
+
+console.log(thinker);
+
+let leader = Math.random() > 0.5 ? "Imran Khan" : 70;
+leader.toString();
+
+console.log(leader);
+
+// ========== Narrowing ========== //
+
+let admiral: number | string;
+admiral = "Grace Hopper";
+
+console.log(admiral);
+console.log(typeof admiral);
+
+let inventor: number | string = "Warren Buffett";
+
+console.log(inventor.toUpperCase());
+
+let scientist = Math.random() > 0.5 ? "Thomas Edison" : 87.6;
+
+if (scientist === "Thomas Edison") {
+    console.log(scientist.toUpperCase());
+}
+else if (scientist === 87.6) {
+    console.log(scientist.toFixed());
+}
