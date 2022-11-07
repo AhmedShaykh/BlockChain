83fc48de644cb96c3d3d39ce364e2efc345e03b7
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-11-07 23:47:14 +0500

    Git Init

--- Node.JS/src/Chapter_2/array.mjs
+++ Node.JS/src/Chapter_2/array.mjs
@@ -23,8 +23,10 @@ arrx[0] = "New Value";
 console.log(arrx[0]);
 
 
-const cars = ["Toyota", "Renault", "Volkswagen"];
+const cars = ["Bugatti", "Civic", "Volkswagen"];
+cars.pop();
 console.log(cars[0]);
+console.log(cars);
 console.log(cars[-1]);
 
 cars[0] = "Tesla";
@@ -32,12 +34,12 @@ console.log(cars[0]);
 
 console.log(cars);
 
-cars[3] = "Kia";
-console.log(cars[3]);
+cars[2] = "Kia";
+cars[2] = "Lambo";
 
 console.log(cars);
 
-cars[-1] = "Fiat";
+cars[-1] = "Ferrari";
 console.log(cars[-1]);
 
 console.log(cars);
@@ -68,5 +70,58 @@ console.log(lengthOfFavoriteFruits);
 console.log(favoriteFruits);
 
 let arrOfShapes = ["circle", "triangle", "rectangle", "pentagon"];
-arrOfShapes.splice(2, 0, "square", "trapezoid");
+arrOfShapes.splice(2, 1, "square", "trapezoid");
+
 console.log(arrOfShapes);
+
+let arr5 = [1, 2, 3];
+let arr6 = [4, 5, 6];
+let arr7 = arr5.concat(arr6);
+console.log(arr7);
+
+let arr8 = arr7.concat(7, 8, 9);
+console.log(arr8);
+
+arr8.shift();
+console.log(arr8);
+
+arr8.splice(1, 3);
+console.log(arr8);
+
+delete arr8[0];
+console.log(arr8);
+
+arr8 = [2, 6, 7, 8];
+let findValue = arr8.find(function (e) { return e === 6 });
+
+let findValue2 = arr8.find(e => e === 10);
+console.log(findValue, findValue2);
+
+let findIndex = arr8.indexOf(6);
+let findIndex2 = arr8.indexOf(10);
+
+console.log(findIndex, findIndex2);
+
+let findIndex3 = arr8.indexOf(6, 0); // (Search + Start)
+console.log(findIndex3);
+
+let animals = ["dog", "horse", "cat", "platypus", "rabbit"];
+let lastDog = animals.lastIndexOf("dog");
+console.log(lastDog);
+
+let names = ["James", "Alicia", "Fatiha", "Maria", "Bert"];
+names.sort();
+console.log(names);
+
+let ages = [18, 72, 33, 56, 40];
+ages.sort();
+console.log(ages.reverse());
+
+let dev = ['Joe', 'Morten', 'David', 'Jack', 'Christian', 'Chris'];
+console.log(dev);
+
+let ceo = dev.slice(0, 4);
+console.log(ceo);
+
+let cto = dev.slice(-3, -1);
+console.log(cto);
