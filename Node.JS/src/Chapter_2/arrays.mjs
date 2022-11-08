eb315f00fef8a03d0df40eeb9f5025313b59b2f4
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-11-08 19:01:32 +0500

    Git Init

--- Node.JS/src/Chapter_2/arrays.mjs
+++ Node.JS/src/Chapter_2/arrays.mjs
@@ -125,3 +125,24 @@ console.log(ceo);
 
 let cto = dev.slice(-3, -1);
 console.log(cto);
+
+let someValues1 = [1, 2, 3];
+let someValues2 = [4, 5, 6];
+let someValues3 = [7, 8, 9];
+let arrOfArrays = [someValues1, someValues2, someValues3];
+
+console.log(arrOfArrays);
+
+let value1 = arrOfArrays[0][1];
+let value2 = arrOfArrays[2][2];
+
+console.log(value1);
+console.log(value2);
+
+let arrOfArraysOfArrays = [arrOfArrays, arrOfArrays, arrOfArrays];
+
+console.log(arrOfArraysOfArrays);
+
+let middleValue = arrOfArraysOfArrays[1][1][1];
+
+console.log(middleValue);
