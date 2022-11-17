c5bcd6068be2cef873ddf471a1ca1961188bca5f
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-11-17 23:55:22 +0500

    Git Init

---
+++ JavaScript/Chapter_13/fetchApi.js
@@ -0,0 +1,30 @@
+async function getAPI() {
+    const api = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    console.log(api, "GET API");
+
+    const json = await api.json();
+    console.log(json, "Json")
+
+}
+
+getAPI();
+
+async function postAPI() {
+    const api = await fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title: 'Learning',
+            body: 'JavaScript',
+            userId: 4,
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8' // <=== Secrect Key or Token
+        }
+    });
+    console.log(api, "POST API");
+
+    const json = await api.json()
+    console.log(json, "Json")
+}
+
+postAPI();
---
+++ JavaScript/Chapter_13/thismethod.js
@@ -0,0 +1,85 @@
+const ladder = {
+    step: 0,
+    stepUp() {
+        this.step++
+        console.log(this.step);
+    },
+    stepDown() {
+        this.step--
+        console.log(this.step);
+    },
+    showStep() {
+        console.log(this.step);
+    }
+}
+
+ladder.stepUp();
+ladder.showStep();
+ladder.stepUp();
+ladder.stepDown();
+
+let myObject = {
+    foo: "bar",
+    func: function () {
+        var self = this;
+        console.log("Outer func:  this.foo = " + this.foo); // bar
+        console.log("Outer func:  self.foo = " + self.foo); // bar
+        (function () {
+            console.log("Inner func:  this.foo = " + this.foo); // undefined
+            console.log("Inner func:  self.foo = " + self.foo); // bar
+        }());
+    }
+};
+
+myObject.func();
+
+let obj1 = {
+    meeting: 0,
+    meetDone: 0,
+    addMeeting(a) {
+        this.meeting = this.meeting + a
+    },
+    summary() {
+        console.log(`You have ${this.meeting} Meetings Left`);
+    },
+    decrementMeet() {
+        this.meeting = this.meeting - 1
+    },
+    decrementSummary() {
+        console.log(`You have ${this.meeting} Meetings Left`);
+    },
+    resetMeeting() {
+        this.meeting = this.meeting = 'No';
+    },
+    meetLeft() {
+        console.log(`You have ${this.meeting} Meeting ...!`);
+    },
+    addMeetDone() {
+        this.meetDone++
+    },
+    summaryMeetDone() {
+        console.log(`You have Done ${this.meetDone} Meeting ...!`);
+    }
+};
+
+obj1.addMeeting(2);
+obj1.addMeeting(2);
+obj1.summary();
+obj1.addMeetDone();
+obj1.summaryMeetDone();
+obj1.decrementMeet();
+obj1.decrementSummary();
+obj1.resetMeeting();
+obj1.meetLeft();
+
+class Solana { // ======== This Method Specialy use in Classes ======== //
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    };
+};
+
+let Coin = new Solana();
+Coin.height = 100;
+Coin.width = 300;
+console.log(Coin);
