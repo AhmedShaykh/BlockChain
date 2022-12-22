579e4a2a15ca796cdc1cbd010b6c14e89d1aef13
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2022-12-22 18:12:09 +0500

    Git Init

--- TypeScript/src/index.ts
+++ TypeScript/src/index.ts
@@ -11,6 +11,7 @@ console.log(undef);
 console.log(nul);
 
 let firstName: string = "Ahmed";
+console.log(firstName);
 console.log(firstName.length);
 
 let boolean: boolean = Math.random() > 0.5 ? true : false;
@@ -39,3 +40,97 @@ export const course: Course = {
 
 console.log(metaVerse);
 console.log(course);
+
+// ========== Unions Types ========== //
+
+let mathematician = Math.random() > 0.5 ? 123 : "Mark Goldberg";
+console.log(mathematician);
+
+let thinker: string | null = null;
+
+if (Math.random() > 0.5) {
+    thinker = "DataScript";
+}
+
+console.log(thinker);
+
+let leader = Math.random() > 0.5 ? "Imran Khan" : 70;
+leader.toString();
+
+console.log(leader);
+
+// ========== Narrowing ========== //
+
+let investor = Math.random() > 0.6 ? "Warren Buffett" : undefined;
+
+if (investor) {
+    investor.toUpperCase();
+};
+
+console.log(investor?.toUpperCase());
+
+let scientist = Math.random() > 0.5 ? "Thomas Edison" : 87.6;
+
+if (scientist === "Thomas Edison") {
+    console.log(scientist.toUpperCase());
+}
+
+else if (scientist === 87.6) {
+    console.log(scientist.toFixed());
+}
+
+let researcher = Math.random() > 0.5 ? "Rosalind Franklin" : 51.08341254;
+
+typeof researcher !== "string" ?
+    console.log(researcher.toFixed()) :
+    console.log(researcher.toUpperCase());
+
+// ========== Literal Types ========== //
+
+let literal: number | "String";
+literal = "String";
+console.log(literal);
+
+literal = 44;
+console.log(literal);
+
+let science: "Computer" | "Chemistry";
+science = Math.random() > 0.5 ? "Computer" : "Chemistry";
+
+if (science == "Computer") {
+    console.log(science.toUpperCase());
+}
+else {
+    console.log(science.toLowerCase());
+}
+
+let biologist = Math.random() > 0.5 && "Rachel Carson"; // String | False
+console.log(biologist);
+
+let program = Math.random() > 0.5 ? "Smart Contract" : undefined;
+console.log(program?.length); //Optional Use Only Undefined In Union Method
+
+// ========== Type Alias ========== //
+
+type RawData = boolean | number | string;
+
+let rawDataFirst: RawData;
+let rawDataSecond: RawData;
+let rawDataThird: RawData;
+
+console.log(rawDataFirst = true);
+console.log(rawDataSecond = "AHMED");
+console.log(rawDataThird = 4000);
+
+type Id = boolean | string;
+type IdMaybe = Id | undefined | null;
+
+let typeFirst: Id;
+let typeSecond: IdMaybe;
+let typeThird: IdMaybe;
+let typeFourth: IdMaybe;
+
+console.log(typeFirst = 'Ahmed');
+console.log(typeSecond);
+console.log(typeThird = null);
+console.log(typeFourth = false);
