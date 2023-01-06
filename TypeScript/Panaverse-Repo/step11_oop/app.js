7f19bdec55c8d084ed124a065732c0056d0962a0
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-01-06 21:17:00 +0500

    Git Init

--- TypeScript/Panaverse-Repo/step11_oop/app.js
+++ TypeScript/Panaverse-Repo/step11_oop/app.js
@@ -1,126 +1,95 @@
 class Data {
-    name: String;
-
-    constructor(name: String) {
+    name;
+    constructor(name) {
         this.name = name;
     }
-};
-
+}
+;
 const show = new Data("Learning OOP In TypeScript");
 console.log(show.name);
-
 // ============= Abstraction ============= //
-
-abstract class Character {
-    public name: string;
-    public damage: number;
-    public attackSpeed: number;
-
-    constructor(name: string, damage: number, speed: number) {
+class Character {
+    name;
+    damage;
+    attackSpeed;
+    constructor(name, damage, speed) {
         this.name = name;
         this.damage = damage;
         this.attackSpeed = speed;
     }
-
-    public abstract damagePerSecond(): number;
 }
-
 class Goblin extends Character {
-    constructor(name: string, damage: number, speed: number) {
+    constructor(name, damage, speed) {
         super(name, damage, speed);
     }
-
-    public damagePerSecond(): number {
+    damagePerSecond() {
         return this.damage * this.attackSpeed;
     }
 }
-
 const abstractData = new Goblin("Martin", 89, 78);
 console.log(abstractData.name);
 console.log(abstractData.damagePerSecond());
-
 // ============= Encapsulation ============= //
-
 class Person {
-    private _name: string;
-
-    constructor(name: string) {
+    _name;
+    constructor(name) {
         this._name = name;
     }
-
-    public get name(): string {
+    get name() {
         return this._name;
     }
-
-    public set name(value: string) {
+    set name(value) {
         this._name = value;
     }
 }
-
 // ============= Inheritance ============= //
-
 class User {
-    public name: string;
-    public damage: number;
-
-    constructor(name: string, damage: number) {
+    name;
+    damage;
+    constructor(name, damage) {
         this.name = name;
         this.damage = damage;
     }
-
-    public talk(): void {
+    talk() {
         console.log('Says something ...');
     }
 }
-
 class Orc extends User {
-    public weapon: string;
-
-    constructor(name: string, damage: number, weapon: string) {
+    weapon;
+    constructor(name, damage, weapon) {
         super(name, damage);
-
         this.weapon = weapon;
     }
-
-    public attack(): void {
+    attack() {
         console.log(`Attacks his target with his ${this.weapon}.`);
     }
 }
-
 // ============= Polymorphism ============= //
-
 class Username {
-    public name: string;
-    public damage: number;
-
-    constructor(name: string, damage: number) {
+    name;
+    damage;
+    constructor(name, damage) {
         this.name = name;
         this.damage = damage;
     }
-
-    public talk(): void {
+    talk() {
         console.log('Says something ...');
     }
-
-    public attack(): void {
+    attack() {
         console.log(`Attacks his target with his fists.`);
     }
 }
-
 class id extends Username {
-    public weapon: string;
-
-    constructor(name: string, damage: number, weapon: string) {
+    weapon;
+    constructor(name, damage, weapon) {
         super(name, damage);
-
         this.weapon = weapon;
     }
-
-    public talk(): void {
+    talk() {
         console.log('Says something but in orcish ...');
     }
-
-    public attack(): void {
+    attack() {
         console.log(`Attacks his target with his ${this.weapon}.`);
     }
 }
+export {};
