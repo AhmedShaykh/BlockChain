7672430be42035fe7b3404dc39f71906ab5605ac
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-04-27 01:03:21 +0500

    Update TypeScript

--- TypeScript/Material/src/index.ts
+++ TypeScript/Material/src/index.ts
@@ -547,450 +547,446 @@ logWarriors("Muslims Warriors", ...warriors);
 
 // ============= Functions ============= //
 
-// function sing(song: string): void {
-//     console.log(`Song: ${song}!`);
-// }
+function sing(song: string): void {
+    console.log(`Song: ${song}!`);
+};
 
-// sing("Happy");
+sing("Happy");
 
-// function announceSong(song: string, singer?: string) {
+function announceSong(song: string, singer?: string) {
 
-//     if (singer) {
-//         console.log(`Song: ${song}, Singer: ${singer}`);
-//     }
-//     else {
-//         console.log(`Song: ${song}`);
-//     }
+    if (singer) {
+        console.log(`Song: ${song}, Singer: ${singer}`);
+    }
+    else {
+        console.log(`Song: ${song}`);
+    }
 
-// };
+};
 
-// announceSong("Dreamer");
-// announceSong("On My Way", "Sabrina Carpenter");
+announceSong("Dreamer");
+announceSong("On My Way", "Sabrina Carpenter");
 
-// function announceSongBy(song: string, singer: string | undefined): void {
+function announceSongBy(song: string, singer: string | undefined): void {
 
-//     if (singer === "string") {
-//         console.log(`Song: ${song}, Singer: ${singer}`);
-//     }
-//     else {
-//         console.log(`Song: ${song}, Singer: ${singer}`);
-//     }
+    if (singer === "string") {
+        console.log(`Song: ${song}, Singer: ${singer}`);
+    }
+    else {
+        console.log(`Song: ${song}, Singer: ${singer}`);
+    }
 
-// };
+};
 
-// announceSongBy("Once Again", undefined);
+announceSongBy("Once Again", undefined);
 
-// function edm(song: string, rating = 5) {
-//     console.log(`${song} Gets Rating ${rating}/10`);
-// };
+function edm(song: string, rating = 8) {
+    console.log(`${song} Gets Rating ${rating}/10`);
+};
 
-// edm("Hardwell - Mad World", 9);
-// edm("Bella Ciao");
-// edm("Will Sparks - Dreaming", 10);
+edm("Hardwell - Mad World", 9);
+edm("Bella Ciao");
+edm("Will Sparks - Dreaming", 10);
 
-// function song(singer: string, ...songs: string[]) {
+function song(singer: string, ...songs: string[]) {
 
-//     for (const var1 of songs) {
-//         console.log(`${var1} Feat. ${singer}`)
-//     }
+    for (const var1 of songs) {
+        console.log(`${var1} Feat. ${singer}`)
+    }
 
-// };
+};
 
-// song("Akcent", "Thats My Name", "Stay With Me", "King Of Disco");
+song("Akcent", "Thats My Name", "Stay With Me", "King Of Disco");
 
-// function technoTrance(techno: string[]) {
+function technoTrance(techno: string[]) {
 
-//     for (let trance of techno) {
-//         console.log(`${trance}`);
-//     };
+    for (let trance of techno) {
+        console.log(`${trance}`);
+    };
 
-// };
+};
 
-// technoTrance(["Will Sparks - L.S.D", "MADDIX - Formula"]);
+technoTrance(["Will Sparks - L.S.D", "MADDIX - Formula"]);
 
-// function getSongAt(songs: string, index: number) {
-//     return index < songs.length ? songs[index] : undefined;
-// };
+function getSongAt(songs: string, index: number) {
+    return index < songs.length ? songs[index] : undefined;
+};
 
-// console.log(getSongAt("Revealed", 4));
+console.log(getSongAt("Revealed", 4));
 
-// function getSongRecordingDate(song: string): Date | undefined | string {
-//     switch (song) {
-//         case "Summertime Sadness":
-//             return new Date();
-//         case "Lockdown":
-//             return "14 August, 2018";
-//         default:
-//             return undefined;
-//     }
-// };
+function getSongRecordingDate(song: string): Date | undefined | string {
+    switch (song) {
+        case "Summertime Sadness":
+            return new Date();
+        case "Lockdown":
+            return "14 August, 2018";
+        default:
+            return undefined;
+    }
+};
 
-// console.log(getSongRecordingDate("Lockdown"));
-// console.log(getSongRecordingDate("Summertime Sadness"));
+console.log(getSongRecordingDate("Lockdown"));
+console.log(getSongRecordingDate("Summertime Sadness"));
 
-// function fun1(callback: Function) {
-//     setTimeout(() => {
-//         callback();
-//     }, 2000);
-//     console.log("Please Wait...");
-// };
+function fun1(callback: Function) {
+    setTimeout(() => {
+        callback();
+    }, 2000);
+    console.log("Please Wait...");
+};
 
-// function fun2() {
-//     console.log("Working Done!")
-// };
+function fun2() {
+    console.log("Working Done!")
+};
 
-// fun1(fun2);
+fun1(fun2);
 
-// let phonk: (genre: string) => string;
+let phonk: (genre: string) => string;
 
-// phonk = function (genre) {
-//     return `InterWood: ${genre.toUpperCase()}!`;
-// };
+phonk = function (genre) {
+    return `InterWood: ${genre.toUpperCase()}!`;
+};
 
-// console.log(phonk("Metamorphosis"));
+console.log(phonk("Metamorphosis"));
 
-// let data1 = ['text2', 'text3', 'text4'];
+let data1 = ['text2', 'text3', 'text4'];
 
-// data1.forEach((data, index) => {
-//     console.log(data, index);
-// });
+data1.forEach((data, index) => {
+    console.log(data, index);
+});
 
-// type StringToNumber = (input: string) => number;
+type StringToNumber = (input: string) => number;
 
-// let stringToNumber: StringToNumber;
+let stringToNumber: StringToNumber;
 
-// stringToNumber = (input) => input.length;
+stringToNumber = (input) => input.length;
 
-// console.log(stringToNumber("Four"));
+console.log(stringToNumber("Four"));
 
-// type NumberToString = (input: number) => string;
+type NumberToString = (input: number) => string;
 
-// function usesNumberToString(NumberToString: NumberToString) {
-//     console.log(`The String is: ${NumberToString(1234)}`);
-// };
+function usesNumberToString(NumberToString: NumberToString) {
+    console.log(`The String is: ${NumberToString(1234)}`);
+};
 
-// usesNumberToString(input => `${input}! Hooray!`);
+usesNumberToString(input => `${input}! Hooray!`);
 
-// type dataS = (abc: number) => number;
+type dataS = (abc: number) => number;
 
-// function funName(data2: dataS): void {
-//     console.log(data2(404));
-// };
+function funName(data2: dataS): void {
+    console.log(data2(404));
+};
 
-// funName((abc) => abc);
+funName((abc) => abc);
 
-// let songLogger: (song: string) => void;
+let songLogger: (song: string) => void;
 
-// songLogger = (song) => {
-//     console.log(`${song}`);
-// };
+songLogger = (song) => {
+    console.log(`${song}`);
+};
 
-// songLogger("Feel It");
+songLogger("Feel It");
 
-// // ========== Overloads ========== //
+// ========== Overloads ========== //
 
-// function overload(a: String, b: String): String;
-// function overload(a: number, b: number): number;
-// function overload(a: string, b: number): string;
-// function overload(a: boolean, b: boolean): boolean;
-// function overload(a: any, b: any): any {
-//     return a + b;
-// };
+function overload(a: String, b: String): String;
+function overload(a: number, b: number): number;
+function overload(a: string, b: number): string;
+function overload(a: boolean, b: boolean): boolean;
+function overload(a: any, b: any): any {
+    return a + b;
+};
 
-// console.log(overload("Hello ", "World"));
-// console.log(overload(112, 88));
-// console.log(overload("Ahmed: ", 1));
-// console.log(overload(true, false));
+console.log(overload("Hello ", "World"));
+console.log(overload(112, 88));
+console.log(overload("Ahmed: ", 1));
+console.log(overload(true, false));
 
 // ============= Tuples ============= //
 
-// let tuple: [number, string] = [4, "SOLANA"];
-// console.log(tuple);
+let tuple: [number, string] = [4, "SOLANA"];
+console.log(tuple);
 
-// tuple[0] = 5;
-// console.log(tuple);
+tuple[0] = 5;
+console.log(tuple);
 
-// let tupleValue: [number, number] = [4, 6];
+let tupleValue: [number, number] = [4, 6];
 
-// console.log(tupleValue);
-// console.log(tupleValue[1]);
+console.log(tupleValue);
+console.log(tupleValue[1]);
 
-// let [year, warrior] = Math.random() > 0.5
-//     ? [340, "Archidamia"]
-//     : [1828, "Rani of Jhansi"];
+let [year, warrior] = Math.random() > 0.5
+    ? [340, "Archidamia"]
+    : [1828, "Rani of Jhansi"];
 
-// console.log(warrior);
-// console.log(year);
+console.log(warrior);
+console.log(year);
 
-// const tupleThree: [boolean, number, string] = [false, 1583, "Ninga"];
-// const tupleTwoExact: [boolean, number] = [tupleThree[0], tupleThree[1]];
+const tupleThree: [boolean, number, string] = [false, 1583, "Ninga"];
+const tupleTwoExact: [boolean, number] = [tupleThree[0], tupleThree[1]];
 
-// console.log(tupleTwoExact);
+console.log(tupleTwoExact);
 
-// const ourTuple: [number, boolean, string] = [5, false, 'Coding God Was Here'];
+function logPair(name: string, value: number) {
+    console.log(`${name} has ${value}`);
+};
 
-// ourTuple.push('Something New & Wrong');
-// console.log(ourTuple);
+const pairTupleCorrect: [string, number] = ["Amenda", 1]; // Order Number Not Change
+logPair(...pairTupleCorrect);
 
-// function logPair(name: string, value: number) {
-//     console.log(`${name} has ${value}`);
-// }
+let readonlyTuple = [111, "Trevor"] as const; // Readonly Not Change Values
 
-// const pairTupleCorrect: [string, number] = ["Amenda", 1]; // Order Number Not Change
-// logPair(...pairTupleCorrect);
+console.log(readonlyTuple);
 
-// let readonlyTuple = [111, "Trevor"] as const; // Readonly Not Change Value
+// ============= Interface ============= //
 
-// console.log(readonlyTuple);
+interface Crypto {
+    supply: number; // Interface Use Structure Of Object
+    name: string;
+};
 
-// // ============= Interface ============= //
+let cryptoCoin: Crypto;
 
-// interface Crypto {
-//     supply: number; // // Interface Use Structure Of Object
-//     name: string;
-// };
+cryptoCoin = {
+    supply: 10000,
+    name: "AHMX Coin",
+};
 
-// let cryptoCoin: Crypto;
+console.log(cryptoCoin);
 
-// cryptoCoin = {
-//     supply: 10000,
-//     name: "AHMX Coin",
-// };
+interface Ethereum {
+    founder: string;
+    price: number;
+    nationality?: string;
+};
 
-// console.log(cryptoCoin);
+let ether: Ethereum = {
+    founder: "Vitalik Buterin",
+    price: 1050,
+    nationality: "Russian Canadian",
+};
 
-// interface Ethereum {
-//     founder: string;
-//     price: number;
-//     nationality?: string;
-// };
+console.log(ether);
 
-// let ether: Ethereum = {
-//     founder: "Vitalik Buterin",
-//     price: 1050,
-//     nationality: "Russian Canadian",
-// };
+interface Topic {
+    readonly text: string;
+    pageNumbers?: number;
+};
 
-// console.log(ether);
+let topic: Topic = {
+    text: "TypeScript", // Assign Value Only One Time Can't Change It!
+    pageNumbers: 343,
+};
 
-// interface Topic {
-//     readonly text: string;
-//     pageNumbers?: number;
-// };
+console.log(topic);
+console.log(topic.text);
+console.log(topic.pageNumbers = 450);
 
-// let topic: Topic = {
-//     text: "TypeScript", // Assign Value Only One Time Can't Change It!
-//     pageNumbers: 343,
-// };
+interface HasBothFunctionTypes {
+    property: () => string;
+    method(): string;
+};
 
-// console.log(topic);
-// console.log(topic.text);
-// console.log(topic.pageNumbers = 450);
+const hasFunction: HasBothFunctionTypes = {
+    property: () => "Edge Computing!",
+    method() {
+        return "Serverless";
+    }
+};
 
-// interface HasBothFunctionTypes {
-//     property: () => string;
-//     method(): string;
-// };
+console.log(hasFunction.method());
+console.log(hasFunction.property());
 
-// const hasFunction: HasBothFunctionTypes = {
-//     property: () => "Edge Computing!",
-//     method() {
-//         return "Serverless";
-//     }
-// };
+interface CallSignature {
+    (input: string): number;
+};
 
-// console.log(hasFunction.method());
-// console.log(hasFunction.property());
+const typedFunctionAlias: CallSignature = (input) => input.length;
 
-// interface CallSignature {
-//     (input: string): number;
-// };
-
-// const typedFunctionAlias: CallSignature = (input) => input.length;
-
-// console.log(typedFunctionAlias("Interface"));
-
-// interface DatesByName {
-//     [i: string]: Date;
-// };
+console.log(typedFunctionAlias("Interface"));
 
-// const publishDates: DatesByName = {
-//     Frankenstein: new Date("1 January 2030"),
-// };
+interface DatesByName {
+    [i: string]: Date;
+};
 
-// publishDates.Frankenstein;
-// console.log(publishDates.Frankenstein.toString());
+const publishDates: DatesByName = {
+    Frankenstein: new Date(),
+};
 
-// interface ChapterStarts {
-//     readonly preface: 0; // Value Fix Interface Only Number Assign
-//     [i: string]: number;
-// };
+console.log(publishDates.Frankenstein.toString());
 
-// const correctPreface: ChapterStarts = {
-//     preface: 0, // Interface Assign Value Than Not Change!
-//     night: 1,
-//     shopping: 5
-// };
+interface ChapterStarts {
+    readonly preface: 0; // Value Fix
+    [i: string]: number;
+};
 
-// console.log(correctPreface.preface);
+const correctPreface: ChapterStarts = {
+    preface: 0, // Interface Assign Value Not Change!
+    night: 1,
+    shopping: 5
+};
 
-// const wrongPreface: ChapterStarts = {
-//     preface: 0,
-// };
+console.log(correctPreface.preface);
 
-// console.log(wrongPreface);
+const wrongPreface: ChapterStarts = {
+    preface: 0,
+};
 
-// interface MoreNarrowNumbers {
-//     [i: number]: string;
-//     [i: string]: string | undefined;
-// };
+console.log(wrongPreface);
 
-// const mixesNumbersAndStrings: MoreNarrowNumbers = {
-//     0: 'One',
-//     1.5: '1.5',
-//     key1: 'Two',
-//     key2: undefined,
-// };
+interface MoreNarrowNumbers {
+    [i: number]: string;
+    [i: string]: string | undefined;
+};
 
-// console.log(mixesNumbersAndStrings);
+const mixesNumbersAndStrings: MoreNarrowNumbers = {
+    0: 'One',
+    1.5: '1.5',
+    key1: 'Two',
+    key2: undefined,
+    5: "Five",
+};
 
-// interface Novel {
-//     author: {
-//         name: string;
-//     };
-//     setting: Setting;
-// };
+console.log(mixesNumbersAndStrings);
 
-// interface Setting {
-//     place: string;
-//     year: number;
-// };
+interface Novel {
+    author: {
+        name: string;
+    };
+    setting: Setting;
+};
 
-// let myNovel: Novel;
+interface Setting {
+    place: string;
+    year: number;
+};
 
-// myNovel = {
-//     author: {
-//         name: 'Jane Austen',
-//     },
-//     setting: {
-//         place: 'England',
-//         year: 1812,
-//     }
-// };
+let myNovel: Novel;
 
-// console.log(myNovel.setting);
+myNovel = {
+    author: {
+        name: 'Jane Austen',
+    },
+    setting: {
+        place: 'England',
+        year: 1812,
+    }
+};
 
-// interface Writinng {
-//     title: string;
-// };
+console.log(myNovel.setting);
 
-// interface Reading extends Writinng {
-//     pages: number;
-// };
+interface Writinng {
+    title: string;
+};
 
-// let myReading: Reading = {
-//     pages: 195,
-//     title: "Ethan Frome",
-// };
+interface Reading extends Writinng {
+    pages: number;
+};
 
-// console.log(myReading);
+let myReading: Reading = {
+    pages: 195,
+    title: "Ethan Frome",
+};
 
-// interface Merged { // Same Name Multi Interface's Allow
-//     fromFirst: string;
-// };
+console.log(myReading);
 
-// interface Merged {
-//     fromSecond: number;
-// };
+interface Merged { // Multi Same Interface's Allow
+    fromFirst: string;
+};
 
-// let merged: Merged = {
-//     fromFirst: "BTC",
-//     fromSecond: 1
-// };
+interface Merged {
+    fromSecond: number;
+};
 
-// console.log(merged);
+let merged: Merged = {
+    fromFirst: "BTC",
+    fromSecond: 1
+};
 
-// // ========== Generics ========== //
+console.log(merged);
 
-// function identity<T>(input: T) {
-//     return input;
-// };
+// ========== Generics ========== //
 
-// const numeric = identity<number>(666);
-// console.log(numeric);
+function identity<T>(input: T) {
+    return input;
+};
 
-// const stringy = identity<string>("Generic");
-// console.log(stringy);
+const numeric = identity<number>(666);
+console.log(numeric);
 
-// function gen<T>(a: T, b: T): T[] {
-//     return [a, b]
-// };
+const stringy = identity<string>("Generic");
+console.log(stringy);
 
-// console.log(gen<number>(2, 2));
-// console.log(gen<string>("2", "2"));
-// console.log(gen<Array<number>>([2], [4]));
+function gen<T>(a: T, b: T): T[] {
+    return [a, b]
+};
 
-// function addUser<T>(user: T) {
-//     return user;
-// };
+console.log(gen<number>(2, 2));
+console.log(gen<string>("2", "2"));
+console.log(gen<Array<number>>([2], [4]));
 
-// console.log(addUser<string>("AHM X ETH"));
+function addUser<T>(user: T) {
+    return user;
+};
 
-// const generic = <T>(input: T) => input;
-// console.log(generic<number>(1234));
+console.log(addUser<string>("AHM X ETH"));
 
-// function makeTuple<First, Second>(first: First, second: Second) {
-//     return [first, second] as const;
-// };
+const generic = <T>(input: T) => input;
+console.log(generic<number>(1234));
 
-// let tupleGeneric = makeTuple<boolean, string>(true, "abc");
-// console.log(tupleGeneric);
+function makeTuple<First, Second>(first: First, second: Second) {
+    return [first, second] as const;
+};
 
-// interface Box<T> {
-//     inside: T;
-// };
+let tupleGeneric = makeTuple<boolean, string>(true, "abc");
+console.log(tupleGeneric);
 
-// let stringyBox: Box<string> = {
-//     inside: "abc",
-// };
+interface Box<T> {
+    inside: T;
+};
 
-// console.log(stringyBox);
+let stringyBox: Box<string> = {
+    inside: "abc",
+};
 
-// class Secret<Key, Value> {
-//     key: Key;
-//     value: Value;
+console.log(stringyBox);
 
-//     constructor(key: Key, value: Value) {
-//         this.key = key;
-//         this.value = value;
-//     };
+class Secret<Key, Value> {
+    key: Key;
+    value: Value;
 
-//     getValue(key: Key): Value | boolean {
-//         return this.key === key
-//             ? this.value
-//             : false;
-//     }
-// };
+    constructor(key: Key, value: Value) {
+        this.key = key;
+        this.value = value;
+    };
 
-// const storage = new Secret<number, string>(12345, "London");
-// console.log(storage);
-// console.log(storage.getValue(1234));
-// console.log(storage.getValue(12345));
+    getValue(key: Key): Value | boolean {
+        return this.key === key
+            ? this.value
+            : false;
+    }
+};
 
-// type Nullish<T> = null | T;
+const storage = new Secret<number, string>(12345, "London");
 
-// let dataGet: Nullish<number> = 786;
-// console.log(dataGet);
+console.log(storage);
+console.log(storage.getValue(1234));
+console.log(storage.getValue(12345));
 
-// interface WithLength {
-//     length: number;
-// };
+type Nullish<T> = null | T;
 
-// function logWithLength<T extends WithLength>(input: T): T {
-//     console.log(`Length: ${input.length}`);
-//     return input;
-// };
+let dataGet: Nullish<number> = 786;
+console.log(dataGet);
 
-// console.log(logWithLength("No one can figure out your worth but you."));
-// console.log(logWithLength([false, true]));
-// console.log(logWithLength({ length: 1234 }));
+interface WithLength {
+    length: number;
+};
+
+function logWithLength<T extends WithLength>(input: T): T {
+    console.log(`Length: ${input.length}`);
+    return input;
+};
+
+console.log(logWithLength("No one can figure out your worth but you."));
+console.log(logWithLength([false, true]));
+console.log(logWithLength({ length: 1234 }));
